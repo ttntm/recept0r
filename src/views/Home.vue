efe015af25c03a89aaa51fe7829742167ba428d4
ttntm <ttntm@pm.me>
2020-05-30 23:30:23 +0200

    vue-clazy-load added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,12 +12,23 @@
           </div>
         </div>
         <transition name="fade">
-          <p v-if="noResults" class="text-center text-secondary mt-3">No results for your search query :(</p>
+          <span v-if="noResults" class="no-results text-center text-secondary mt-5">No results for your search query :(</span>
         </transition>
       </div>
       <transition-group name="list" tag="div" class="recipe-grid">
         <div v-for="recipe in recipesReversed" :key="recipe.refId" class="recipe-card border border-secondary rounded">
-          <div v-if="recipe.image" class="recipe-card-img border-bottom border-secondary bg-light" v-bind:style="{ 'background-image': 'url(' + recipe.image + ')' }"><!-- image --></div>
+          <clazy-load v-if="recipe.image" :src="recipe.image" class="clazy-container position-relative border-bottom border-secondary" is="VueClazyLoad">
+            <transition name="fade">
+              <img class="recipe-card-img" :src="recipe.image" crossorigin="anonymous">
+            </transition>
+            <transition name="fade" slot="placeholder">
+              <div class="preloader">
+                <div class="circle">
+                  <div class="circle-inner"></div>
+                </div>
+              </div>
+            </transition>
+          </clazy-load>
           <div class="p-4">
             <h3 class="font-weight-bold text-dark">{{ recipe.title }}</h3>
             <p class="text-secondary my-3">{{ recipe.description }}</p>
@@ -31,8 +42,13 @@
 </template>
 
 <script>
+import { VueClazyLoad } from 'vue-clazy-load';
+
 export default {
   name: 'all-recipes',
+  components: {
+    VueClazyLoad
+  },
   props: {
     updateList: Boolean,
     fPath: Object
@@ -101,12 +117,16 @@ export default {
   .search {
     width: 50%;
   }
+  .no-results {
+    position: absolute; /* prevent the page jumping from inserted <p> */
+    left: 0;
+    right: 0;
+  }
   .fade-enter-active, .fade-leave-active {
     transition: all .5s;
   }
-  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  .fade-enter, .fade-leave-to {
     opacity: 0;
-    transform: translateY(30px);
   }
   .recipe-grid {
     display: grid;
@@ -125,10 +145,13 @@ export default {
     box-shadow: 0 0.45rem 0.9rem rgba(0,0,0,.1);
     transition: all .35s ease;
   }
+  .clazy-container {
+    height: 300px;
+  }
   .recipe-card-img {
-    background-size:cover;
-    background-position: center;
-    height:300px;
+    object-fit: cover;
+    width: 100%;
+    max-height:300px;
     opacity: 0.75;
     transition: all .5s ease-in-out;
   }
@@ -139,11 +162,56 @@ export default {
   .list-enter-active, .list-leave-active {
     transition: all .5s;
   }
-  .list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  .list-enter, .list-leave-to {
     opacity: 0;
-    /* transform: translateY(30px); */
   }
   .list-move {
     transition: transform .5s;
   }
+  /* preloader source: https://codepen.io/Alex-Miller/pen/qviHa */
+  .preloader {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin: -25px 0 0 -25px;
+    width: 50px;
+    height: 50px;
+    transition: all 100ms ease-in;
+  }
+
+  .preloader .circle,
+  .preloader .circle .circle-inner {
+    width: inherit;
+    height: inherit;
+    border-radius: 50%;
+    background: linear-gradient(rgba(220, 220, 220, 1) -50%,rgba(220, 220, 220, 0) 60%);
+  }
+
+  .preloader .circle {
+    animation: roll 6s linear infinite;
+  }
+
+  .preloader .circle .circle-inner {
+    padding: 5px;
+    animation: roll 2s linear infinite reverse;
+  }
+
+  .preloader .circle .circle-inner::after {
+    content: '';
+    display: block;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background: #fff;
+  }
+
+  @keyframes roll {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
 </style>
