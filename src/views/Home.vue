f24f6c0a82a75613dbe021c13190c103a53df708
ttntm <ttntm@pm.me>
2020-07-28 18:53:30 +0200

    update: colors in SVGs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@
       <div class="w-full xl:w-2/3 flex flex-row justify-center mb-12 mx-auto">
         <div class="search flex flex-row items-center flex-1" :class="{ 'input-group': searchTerm }">
           <label for="search-input" class="sr-only">Search</label>
-          <svg xmlns="http://www.w3.org/2000/svg" class="flex ml-4" width="25" height="25" viewBox="0 0 24 24" stroke-width="1.5" stroke="#234CAD" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <svg xmlns="http://www.w3.org/2000/svg" class="flex ml-4" width="25" height="25" viewBox="0 0 24 24" stroke-width="1.5" stroke="#30529b" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z"/>
             <circle cx="10" cy="10" r="7" />
             <line x1="21" y1="21" x2="15" y2="15" />
@@ -32,7 +32,7 @@
           aria-label="Select Filter"
         >
           <span class="block lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" class="" width="25" height="25" viewBox="0 0 24 24" stroke-width="1.5" stroke="#234CAD" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <svg xmlns="http://www.w3.org/2000/svg" class="" width="25" height="25" viewBox="0 0 24 24" stroke-width="1.5" stroke="#30529b" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path stroke="none" d="M0 0h24v24H0z"/>
               <path d="M5.5 5h13a1 1 0 0 1 0.5 1.5L14 12L14 19L10 16L10 12L5 6.5a1 1 0 0 1 0.5 -1.5" />
             </svg>
